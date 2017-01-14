4e97b4976794f9f0addf398c8f32ae43a3f6f0fc
eminthwu <andrew.liu@advantech.com.tw>
2017-01-15 01:08:40 +0800

    PriceCalc_第一到三集各一本_270元 passed

--- PotterShoopinCart/ShoppingCart.cs
+++ PotterShoopinCart/ShoppingCart.cs
@@ -11,9 +11,15 @@ namespace PotterShoopinCart
         public int PriceCalc()
         {
             var price = this.Books.Select(b => b.Price).Sum(p => p);
+
+
             if (Books.Count == 2 && Books.Select(b => b.Numero).Distinct().Count() == 2)
             {
                 price = (int)Math.Round(price * 0.95, 0, MidpointRounding.AwayFromZero);
+            }
+            else if(Books.Count == 3 && Books.Select(b=>b.Numero).Distinct().Count() == 3)
+            {
+                price = (int)Math.Round(price * 0.9, 0, MidpointRounding.AwayFromZero);
             }
 
             return price;
