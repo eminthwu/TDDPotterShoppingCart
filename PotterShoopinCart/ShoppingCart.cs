45bc270667084bb45e6d52473daee3e10a79c494
eminthwu <andrew.liu@advantech.com.tw>
2017-01-15 00:38:53 +0800

    PriceCalc_第一集x1_100元 passed

---
+++ PotterShoopinCart/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PotterShoopinCart
+{
+    public class PotterShoopinCart
+    {
+        public List<HarryPotter> Books { get; set; }
+
+        public int PriceCalc()
+        {
+            var price = this.Books.Select(b => b.Price).Sum(p => p);
+           
+
+            return price;
+        }
+    }
+
+    public class HarryPotter
+    {
+        public int Numero { get; set; }
+
+        public int Price { get; } = 100;
+    }
+}
