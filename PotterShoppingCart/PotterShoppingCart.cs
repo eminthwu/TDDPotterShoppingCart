2d25ac3c31fd85369aaefa98318f2450da8f50ca
eminthwu <andrew.liu@advantech.com.tw>
2017-01-16 20:35:54 +0800

    第二個測試案例 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190 綠燈

--- PotterShoppingCart/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -13,6 +13,11 @@ namespace PotterShoppingCart
 
         public int GetPrice()
         {
+            if (Books.Count == 2 && Books.Select(b => b.Seq).Distinct().Count() == 2)
+            {
+                return (int)Math.Round(Books.Sum(b => b.Price) * 0.95, 1, MidpointRounding.AwayFromZero);
+            }
+
             return this.Books.Sum(b => b.Price);
         }
     }
